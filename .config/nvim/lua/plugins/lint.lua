f47198c20cebf3324c118b3104adc8fea58b74fd
Ryan Cavicchioni <ryan@cavi.cc>
2024-10-13 18:52:29 -0500

    Configure nvim-lint

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -1,3 +1,14 @@
 return {
 	"mfussenegger/nvim-lint",
+	config = function()
+		require("lint").linters_by_ft = {
+			ruby = { "rubocop", "standardrb" },
+		}
+
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
+	end,
 }
