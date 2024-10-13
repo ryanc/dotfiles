24c9878ee7d4eb60cac54e7e26276df3d5f676a0
Ryan Cavicchioni <ryan@cavi.cc>
2024-10-13 18:46:54 -0500

    Configure conform key binding

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,18 @@
 return {
 	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
